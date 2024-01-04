6f5bfe5efc931aae88e9516557c04a31cb6094ef
brunomry <brunomadozzo@gmail.com>
2024-01-04 17:00:07 -0300

    Agregue estilos css para hover de imagen y boton de cerrar imagen. Ademas agregue la logica de abrir y cerrar cada imagen mediante 2 funciones haciendo uso del evento click

---
+++ JS/galeria.js
@@ -0,0 +1,26 @@
+const div = document.getElementById('fullscreen');
+const img = document.getElementById('fullscreenImg');
+const botonCerrar = document.getElementById('cerrar');
+
+const abrirImagen = imageSrc => {
+    img.src = imageSrc;
+    div.classList.add('d-flex','flex-column');    
+}
+
+const cerrarImagen = () => {
+  botonCerrar.style.display = 'none';
+  div.classList.remove('d-flex');
+}
+
+document.addEventListener('click', (e) => {
+  if (e.target.closest('.containerBg')) {
+    abrirImagen(e.target.closest('.containerImg').querySelector('img').src);
+    botonCerrar.style.display = 'block';
+}
+
+  if (e.target.id === "cerrar"){
+    cerrarImagen();
+  }
+});
+
+const containerBg = document.getElementById("containerBg")
