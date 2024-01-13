af999cf3d9d4f40f64eb99bc86a65c9d1339d2f9
brunomry <brunomadozzo@gmail.com>
2024-01-13 14:50:13 -0300

    Cambie algunas imagenes de la galeria, y algunos estilos css para que sea responsive. Tambien agregue un boton cuando la imagen se vea en detalle para poder cerrar. Agregue tipografia y etiquetas meta description, keywords y author con sus correspondientes descripciones.

--- JS/galeria.js
+++ JS/galeria.js
@@ -16,9 +16,14 @@ document.addEventListener('click', (e) => {
   if (e.target.closest('.containerBg')) {
     abrirImagen(e.target.closest('.containerImg').querySelector('img').src);
     botonCerrar.style.display = 'block';
-}
+  }
 
-  if (e.target.id === "cerrar"){
+  if (
+    e.target.id === "cerrar" || 
+    e.target.id === "fullscreenImg" || 
+    e.target.id === "btnVolver" || 
+    e.target.id === "fullscreen"
+  ){
     cerrarImagen();
   }
 });
