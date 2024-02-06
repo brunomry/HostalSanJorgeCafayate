eda0514d75b2ab7f38c3f978b71f339dfbfd9d8b
brunomry <brunomadozzo@gmail.com>
2024-02-06 19:58:07 -0300

    Corregi ruta de pagina de regreso en archivo js de input del formulario de consulta de la pagina comoLlegar

--- JS/form.js
+++ JS/form.js
@@ -1,3 +1,6 @@
 const formConsulta = document.getElementById('formQuestions');
+const paginaDeRegreso = document.getElementById('inputNext');
+
+paginaDeRegreso.value = window.location.href;
 
 formConsulta.action = "https://formsubmit.co/hostalsanjorgecafayate@gmail.com";
