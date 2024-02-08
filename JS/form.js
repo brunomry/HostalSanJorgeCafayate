fc97577f8e0f5a918c15269dc48b8cb76ab91536
brunomry <brunomadozzo@gmail.com>
2024-02-08 18:25:44 -0300

    Corregi estilo css en archivo comoLlegar.css

--- JS/form.js
+++ JS/form.js
@@ -3,13 +3,4 @@ const paginaDeRegreso = document.getElementById('inputNext');
 
 paginaDeRegreso.value = window.location.href;
 
-
 formConsulta.action = "https://formsubmit.co/hostalsanjorgecafayate@gmail.com";
-
-if(formConsulta.action){
-  Swal.fire({
-    icon: "success",
-    title: "Tu consulta fue enviada con éxito! En breve recibirás una respuesta.",
-    showConfirmButton: true,
-  });
-}
