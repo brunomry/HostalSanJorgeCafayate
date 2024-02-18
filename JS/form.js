6787823d0d875b1509f7fa71ce17b5c3adcd979b
brunomry <brunomadozzo@gmail.com>
2024-02-18 00:48:02 -0300

    Mimifique archivos css y js para optimizar rendimiento usando la herramienta minifier.org

--- JS/form.js
+++
@@ -1,6 +0,0 @@
-const formConsulta = document.getElementById('formQuestions');
-const paginaDeRegreso = document.getElementById('inputNext');
-
-paginaDeRegreso.value = window.location.href;
-
-formConsulta.action = "https://formsubmit.co/hostalsanjorgecafayate@gmail.com";
