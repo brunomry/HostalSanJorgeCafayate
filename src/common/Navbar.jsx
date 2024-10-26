fe13c820ebc12553a8f28805f75bdbf5283fc552
Bruno Madozzo <brunomadozzo@gmail.com>
2024-10-26 02:57:11 -0300

    agregue btn de hamburguesa en navbar y modifique opcion de idioma

--- src/common/Navbar.jsx
+++ src/common/Navbar.jsx
@@ -1,166 +1,116 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import es from "../assets/IMG/argentina.webp";
+import en from "../assets/IMG/ingles.webp";
 
 const Navbar = () => {
   return (
-    <nav className=" bg-white z-50 dark:bg-gray-900 fixed w-full top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-center mx-auto p-8">
-       
-
-        <div
-          className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-          id="navbar-sticky"
-        >
-          <ul className="flex text-[18px] text-white flex-col items-center p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
+    <header>
+      <nav className=" bg-white z-50 dark:bg-gray-900 fixed w-full top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+        <div className="max-w-screen-xl flex flex-wrap lg:justify-center items-center justify-between mx-auto p-8">
+          <Link
+            to={"/"}
+            className="flex flex-col items-center lg:hidden space-x-3 rtl:space-x-reverse px-5"
+          >
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              Hostal San Jorge
+            </span>
+            <small className="text-gray-600">Cafayate</small>
+          </Link>
+          <button
+            data-collapse-toggle="navbar-hamburger"
+            type="button"
+            className="lg:hidden inline-flex items-center justify-center p-2 w-10 h-10 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-hamburger"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
+          <div
+            className="items-center justify-between hidden w-full lg:flex lg:w-auto md:order-1"
+            id="navbar-hamburger"
+          >
+            <ul className="flex flex-col justify-center text-[18px] text-white lg:flex-row items-center p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                <Link
+                  to={"/"}
+                  className="block py-2 px-3 border-b-[3px] border-b-[#fff] hover:border-b-[3px] hover:border-b-[#0692a2] text-gray-600 hover:text-[#000] rounded "
+                  aria-current="page"
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to={"/habitaciones"}
+                  className="block py-2 px-3 rounded border-b-[3px] border-b-[#fff] hover:border-b-[3px] hover:border-b-[#0692a2] text-gray-600 hover:text-[#000]"
+                >
+                  Habitaciones
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to={"/nosotros"}
+                  className="block py-2 px-3 rounded border-b-[3px] border-b-[#fff] hover:border-b-[3px] hover:border-b-[#0692a2] text-gray-600 hover:text-[#000]"
+                >
+                  Nosotros
+                </Link>
+              </li>
               <Link
                 to={"/"}
-                className="block py-2 px-3  border-b-[3px] border-b-[#fff] hover:border-b-[3px] hover:border-b-[#0692a2] text-gray-600 hover:text-[#000] rounded "
-                aria-current="page"
+                className="hidden lg:flex flex-col items-center space-x-3 rtl:space-x-reverse px-5"
               >
-                Home
+                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                  Hostal San Jorge
+                </span>
+                <small className="text-gray-600">Cafayate</small>
               </Link>
-            </li>
-            <li>
-              <Link
-                to={"/habitaciones"}
-                className="block py-2 px-3 rounded border-b-[3px] border-b-[#fff] hover:border-b-[3px] hover:border-b-[#0692a2] text-gray-600 hover:text-[#000]"
-              >
-                Habitaciones
-              </Link>
-            </li>
-            <li>
-              <Link
-                to={"/nosotros"}
-                className="block py-2 px-3 rounded border-b-[3px] border-b-[#fff] hover:border-b-[3px] hover:border-b-[#0692a2] text-gray-600 hover:text-[#000]"
-              >
-                Nosotros
-              </Link>
-            </li>
-            <Link
-          to={"/"}
-          className="flex flex-col items-center space-x-3 rtl:space-x-reverse px-5"
-        >
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Hostal San Jorge
-          </span>
-          <small className="text-gray-600">Cafayate</small>
-        </Link>
-            <li>
-              <Link
-                to={"/galeria"}
-                className="block py-2 px-3 rounded border-b-[3px] border-b-[#fff] hover:border-b-[3px] hover:border-b-[#0692a2] text-gray-600 hover:text-[#000]"
-              >
-                Galería
-              </Link>
-            </li>
-            <li>
-              <Link
-                to={"/contacto"}
-                className="block py-2 px-3 rounded border-b-[3px] border-b-[#fff] hover:border-b-[3px] hover:border-b-[#0692a2] text-gray-600 hover:text-[#000]"
-              >
-                Contacto
-              </Link>
-            </li>
-            <li>
-              <Link
-                to={"/"}
-                className="block py-2 px-3 rounded border-b-[3px] border-b-[#fff] hover:border-b-[3px] hover:border-b-[#0692a2] text-gray-600 hover:text-[#000]"
-              >
-                Idioma
-              </Link>
-            </li>
-          </ul>
+              <li>
+                <Link
+                  to={"/galeria"}
+                  className="block py-2 px-3 rounded border-b-[3px] border-b-[#fff] hover:border-b-[3px] hover:border-b-[#0692a2] text-gray-600 hover:text-[#000]"
+                >
+                  Galería
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to={"/contacto"}
+                  className="block py-2 px-3 rounded border-b-[3px] border-b-[#fff] hover:border-b-[3px] hover:border-b-[#0692a2] text-gray-600 hover:text-[#000]"
+                >
+                  Contacto
+                </Link>
+              </li>
+              <li className="flex">
+                <button className="py-2 px-2 flex gap-1 items-center cursor-pointer text-gray-600 hover:text-[#000]">
+                  <span>Es</span>
+                  <img src={es} className="w-[25px] h-[18px] object-cover" />
+                </button>
+                <button className="py-2 px-2 flex gap-1 items-center cursor-pointer text-gray-600 hover:text-[#000]">
+                  <span>En</span>
+                  <img src={en} className="w-[25px] h-[18px] object-cover" />
+                </button>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
-
-    // <nav className="navbar navbar-expand-lg py-lg-0 navBar">
-    //     <div
-    //       className="container-fluid d-flex align-items-center flex-lg-row justify-content-lg-evenly"
-    //     >
-    //       <Link className="navbar-brand ms-xxl-5" href="${index}index.html" title="Logo de Hostal San Jorge"
-    //         >Hostal San Jorge</Link>
-    //       <button
-    //         className="navbar-toggler"
-    //         type="button"
-    //         data-bs-toggle="collapse"
-    //         data-bs-target="#navbarNav"
-    //         aria-controls="navbarNav"
-    //         aria-expanded="false"
-    //         aria-label="Toggle navigation"
-    //       >
-    //         <span className="navbar-toggler-icon"></span>
-    //       </button>
-    //       <div
-    //         className="collapse navbar-collapse my-2 d-lg-flex justify-content-center justify-content-xl-end me-xxl-5"
-    //         id="navbarNav"
-    //       >
-    //         <ul
-    //           className="navbar-nav d-flex gap-2 align-items-center justify-content-lg-end gap-xl-4 my-xl-2 lead navigation"
-    //         >
-    //           <li className="nav-item text-center">
-    //             <Link className="nav-link pb-0 pb-sm-2" href="${index}index.html">Home</Link>
-    //           </li>
-    //           <li className="nav-item text-center">
-    //             <Link className="nav-link" href="${rutaPagina}habitaciones/habitaciones.html">Habitaciones</Link>
-    //           </li>
-    //           <li className="nav-item text-center">
-    //             <Link className="nav-link" href="${rutaPagina}galeria/galeria.html">Galería</Link>
-    //           </li>
-    //           <li className="nav-item text-center">
-    //             <Link className="nav-link" href="${rutaPagina}comoLlegar/comoLlegar.html">Cómo Llegar</Link>
-    //           </li>
-    //           <li className="nav-item text-center">
-    //             <Link className="nav-link" href="${rutaPagina}preguntasFrecuentes/preguntasFrecuentes.html"
-    //               >Preguntas Frecuentes</a
-    //             >
-    //           </li>
-    //           <li className="nav-item dropdown text-center">
-    //             <Link
-    //               className="nav-link dropdown-toggle"
-    //               to={"/"}
-    //               role="button"
-    //               data-bs-toggle="dropdown"
-    //               aria-expanded="false"
-    //             >
-    //               <i className="fa-solid fa-globe"></i
-    //               ><span className="ms-2">Idioma</span>
-    //             </Link>
-    //             <ul className="dropdown-menu lead text-center text-lg-start">
-    //               <li>
-    //                 <Link
-    //                   className="dropdown-item text-secondary fw-light"
-    //                   to={"/"}
-    //                   id="optionEs"
-    //                   ><img
-    //                     src="../../IMG/argentina.webp"
-    //                     className="ms-3 ms-lg-0"
-    //                     alt="bandera de Argentina"
-    //                     title="Idioma Español"
-    //                   />
-    //                   Español</a
-    //                 >
-    //               </li>
-    //               <li>
-    //                 <Link
-    //                   className="dropdown-item text-secondary fw-light"
-    //                   to={"/"}
-    //                   id="optionEn"
-    //                   ><img
-    //                     src="../../IMG/ingles.webp"
-    //                     alt="bandera de Inglaterra"
-    //                     title="Idioma Inglés"
-    //                   />
-    //                   Inglés</a
-    //                 >
-    //               </li>
-    //             </ul>
-    //           </li>
-    //         </ul>
-    //       </div>
-    //     </div>
-    //   </nav>
+      </nav>
+    </header>
   );
 };
 
