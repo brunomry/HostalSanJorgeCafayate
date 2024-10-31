7374aa1e10307868baf9bd7b09b6c3c7612070fe
Bruno Madozzo <brunomadozzo@gmail.com>
2024-10-31 12:29:23 -0300

    importe flowbite-react y plugin en archivo de config de tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
+const flowbite = require("flowbite-react/tailwind");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js"
+    "./node_modules/flowbite/**/*.js",
+    flowbite.content()
   ],
   theme: {
     extend: {},
@@ -19,7 +22,7 @@ export default {
     }
   },
   plugins: [
-    require('flowbite/plugin')
+    flowbite.plugin()
   ],
 }
 
