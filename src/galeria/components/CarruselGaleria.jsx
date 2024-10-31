5d86c14b9caa688032e6df1b4e8ec5c24321e4fb
Bruno Madozzo <brunomadozzo@gmail.com>
2024-10-31 13:28:59 -0300

    quite btn cerrar de carrusel y agregue clase a imagen y botones prev y next de carrusel para que no se cierre al hacer clic en estos

--- src/galeria/components/CarruselGaleria.jsx
+++ src/galeria/components/CarruselGaleria.jsx
@@ -4,7 +4,15 @@ import { RiCloseLargeFill } from "react-icons/ri";
 const CarruselGaleria = ({ setVisible, visible, imagenesActuales, imgActual  }) => {
   const [posicionActual, setPosicionActual] = useState(imgActual);
 
-  const closeCarousel = () => {
+  const closeCarousel = (e) => {
+    if(e.target.matches(".noClose")){
+      return;
+    }
+
+    if(e.target.matches(".noClose *")){
+      return;
+    }
+
     setVisible(false);
   };
 
@@ -25,35 +33,36 @@ const CarruselGaleria = ({ setVisible, visible, imagenesActuales, imgActual  }) 
       id="controls-carousel"
       className="z-50 fixed inset-0 h-[100vh] bg-[rgba(0,0,0,0.9)] w-full flex items-center justify-center"
       data-carousel="static"
+      onClick={(e)=>closeCarousel(e)}
     >
-      <div className="relative w-full max-w-[75%] h-[55%] lg:h-[75%] flex justify-center">
+      <div className="relative w-full max-w-[75%] h-[55%] lg:h-[75%] flex justify-center" >
             <div
               className="h-[100%] flex justify-center items-center"
               data-carousel-item
             >
               <img
                 src={imagenesActuales[posicionActual].url}
-                className=" block absolute max-w-full object-cover max-h-full border-[5px] md:border-[15px]"
+                className="noClose block absolute max-w-full object-cover max-h-full border-[5px] md:border-[15px]"
                 alt="..."
               />
             </div>
         
       </div>
-      <button
-        className="absolute top-[20px] mb:top-[100px] right-[70px] md:top-[200px] md:right-[100px] lg:top-4 2xl:right-[250px] 2xl:top-[75px] z-30 text-white text-2xl cursor-pointer"
-        onClick={() => closeCarousel()}
+      {/* <button
+        className="absolute hidden md:block top-[20px] mb:top-[100px] right-[70px] md:top-[200px] md:right-[100px] lg:top-4 2xl:right-[250px] 2xl:top-[75px] z-30 text-white text-2xl cursor-pointer"
+        onClick={(e)=>closeCarousel(e)}
       >
         <RiCloseLargeFill className="text-[20px] xl:text-[30px]" />
-      </button>
+      </button> */}
       <button
         type="button"
-        className="absolute z-30 top-0 start-0 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none lg:hover:bg-gray-500"
+        className="noClose absolute top-0 left-0 z-30 flex items-center justify-center md:w-[100px] lg:w-[200px] 2xl:w-[400px] translate-y-[40vh] lg:translate-y-[30vh] 2xl:translate-y-[40vh] h-[200px] px-4 cursor-pointer group focus:outline-none lg:hover:bg-[rgba(0,0,0,0.3)]"
         data-carousel-prev
         onClick={handlePrev}
       >
-        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+        <span className="noClose inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
           <svg
-            className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+            className="w-4 noClose h-4 text-white dark:text-gray-800 rtl:rotate-180"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -72,11 +81,11 @@ const CarruselGaleria = ({ setVisible, visible, imagenesActuales, imgActual  }) 
       </button>
       <button
         type="button"
-        className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none lg:hover:bg-gray-500"
+        className="noClose absolute top-0 end-0 z-30 flex items-center justify-center md:w-[100px] lg:w-[200px] 2xl:w-[400px] translate-y-[40vh] lg:translate-y-[30vh] 2xl:translate-y-[40vh] h-[200px] px-4 cursor-pointer group focus:outline-none lg:hover:bg-[rgba(0,0,0,0.3)]"
         data-carousel-next
         onClick={handleNext}
       >
-        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+        <span className="noClose inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
           <svg
             className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
             aria-hidden="true"
