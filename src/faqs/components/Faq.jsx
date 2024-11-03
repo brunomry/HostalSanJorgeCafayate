c1a01c80261e7d962c5d5d0b4784cbcd94192be3
Bruno Madozzo <brunomadozzo@gmail.com>
2024-11-03 02:29:02 -0300

    quite borde de faqs para dispositivos moviles

--- src/faqs/components/Faq.jsx
+++ src/faqs/components/Faq.jsx
@@ -5,7 +5,7 @@ function Faq({ faq }) {
     <>
       <button
         type="button"
-        className="w-[100%] border rounded-[5px] md:border-t-0 md:border-s-0 md:border-r-0 md:border-b flex items-center justify-between md:w-[80%] lg:w-[70%] p-3 md:p-5  text-gray-500 font-medium border-b border-gray-300 focus:ring-gray-200 dark:focus:ring-gray-800  dark:text-gray-400 hover:bg-gray-100 hover:text-[#222] gap-3 "
+        className="w-[100%] rounded-[5px] border-t-0 md:border-s-0 md:border-r-0 md:border-b flex items-center justify-between md:w-[80%] lg:w-[70%] p-3 md:p-5  text-gray-500 font-medium border-b border-gray-300 focus:ring-gray-200 dark:focus:ring-gray-800  dark:text-gray-400 hover:bg-gray-100 hover:text-[#222] gap-3 "
         data-accordion-target={`#${faq.id}`}
         aria-expanded="false"
         aria-controls={`${faq.id}`}
--- src/home/components/BannerHome.jsx
+++ src/home/components/BannerHome.jsx
@@ -13,7 +13,7 @@ function BannerHome() {
           <small className="hidden md:block text-[#fff] mb-0 text-sm">Provincia de Salta, Noroeste Argentino</small>
         <h1 className="text-[.9rem] md:block md:text-[25px] lg:text-[45px] text-[#fff] mt-0 md:font-bold">Hostal San Jorge Cafayate</h1>
         <p className="text-[18px] text-[#fff] lg:text-[20px] font-normal">Tu lugar ideal para descansar y pasar tus vacaciones.</p>
-        <Link to={"/habitaciones"} className="p-3 vsm:text-[.9rem] mb:text-[1rem] vsm:w-[90%] mb:w-[75%] text-[14px] rounded-[5px] md:rounded-none md:p-4 md:w-[100%] flex gap-3 justify-center items-center md:text-[1rem] bg-amber-400  md:mt-8 text-[#0e2541] md:max-w-[270px] lg:hover:bg-gray-950 lg:hover:text-[#fff] xl:p-5">
+        <Link to={"/habitaciones"} className="vsm:px-2 mb:p-3 vsm:text-[.9rem] mb:text-[1rem] vsm:w-[100%] mb:w-[75%] text-[14px] rounded-[5px] md:rounded-none md:p-4 md:w-[100%] flex gap-3 justify-center items-center md:text-[1rem] bg-amber-400  md:mt-8 text-[#0e2541] md:max-w-[270px] lg:hover:bg-gray-950 lg:hover:text-[#fff] xl:p-5">
             <span>NUESTRAS HABITACIONES</span>  <MdArrowForwardIos />   
         </Link>
         <small className="hidden md:block text-sm text-[#fff] font-normal">Estamos a 2 cuadras de la plaza principal.</small>
