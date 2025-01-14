d4151fe24f4468081fc5227c1801a10d1776090f
Bruno Madozzo <brunomadozzo@gmail.com>
2025-01-14 12:14:24 -0300

    actualice comentarios

--- src/pages/home/components/CardComentario.jsx
+++ src/pages/home/components/CardComentario.jsx
@@ -1,21 +1,26 @@
 import { RiStarSFill } from "react-icons/ri";
 
-function CardComentario({ comentario }) {
+function CardComentario({ comentario, index }) {
   const verComentario = (ruta) => window.open(ruta, "_BLANK");
 
   return (
     <div
       onClick={() => verComentario(comentario.ruta)}
-      className="p-3 shadow-md md:p-5 mb-4 text-justify card cursor-pointer bg-slate-50 border min-w-[250px] lg:w-[280px] xl:w-[400px] flex flex-col justify-between items-center gap-3 xl:min-h-[100px] rounded-[8px] xl:p-10 hover:border-[#c5c5c5]"
+      className={`${index===3 ? "md:hidden 3xl:flex": "3xl:flex"} p-3 shadow-md md:p-5 mb-4 text-justify card cursor-pointer bg-slate-50 border min-w-[250px] lg:w-[280px] xl:w-[400px] flex flex-col justify-between items-center gap-3 xl:min-h-[100px] rounded-[8px] xl:p-10 hover:border-[#c5c5c5]`}
       title="clic para ver reseña en Google"
     >
       <div className="flex flex-col items-center md:items-start w-[100%] gap-3">
         <div className="flex justify-center md:justify-start w-[100%]">
-          {[...Array(comentario.estrellas)].map((_, index) => (
-            <RiStarSFill key={index} className="text-[20px] text-yellow-500" />
+          {[...Array(5)].map((_, index) => (
+            <RiStarSFill
+              key={index}
+              className={`text-[20px] ${
+                index < comentario.estrellas ? "text-yellow-500" : "text-gray-300"
+              }`}
+            />
           ))}
         </div>
-        <p className="text-gray-500 ">{comentario.comentario}</p>
+        <p className="text-gray-500">{comentario.comentario}</p>
       </div>
       <div className="flex flex-col items-center md:items-end w-[100%]">
         <p className="text-center md:text-end">{comentario.nombre}</p>
--- src/pages/home/helpers/comentarios.js
+++ src/pages/home/helpers/comentarios.js
@@ -1,29 +1,38 @@
 const comentarios = [
-    {
-      id: 1,
-      nombre: "Graciela Caramanica",
-      comentario: "Muy buena atención de los dueños, muy amables y predispuestos. Las habitaciones cómodas y luminosas, la nuestra con balcón. A 3 cuadras del centro y al lado de una casa de empanadas excelente. Óptima relación precio y calidad. Los dueños nos solucionaron todo. Rico desayuno y hermoso el salón.",
-      fecha: "Hace 4 meses",
-      estrellas: 5,
-      ruta: "https://g.co/kgs/jFcRAJU"
-    },
-    {
-      id: 2,
-      nombre: "Johanna Acosta",
-      comentario: "Todo estuvo perfecto, el dueño está presente lo que hace que todo sea más cálido. La habitación, camas, baño todo super limpio, prolijo y cómodo. Pasamos una linda estadía. Relación precio/servicio super bien.",
-      fecha: "Hace 8 meses",
-      estrellas: 5,
-      ruta: "https://g.co/kgs/quEGv1r"
-    },
-    {
-      id: 3,
-      nombre: "Ana Maria Romero",
-      comentario: "Excelente servicio por su propia dueña.",
-      fecha: "Hace 4 meses",
-      estrellas: 5,
-      ruta: "https://g.co/kgs/JNW7ZWL"
-    }
-  ];
-  
-  export default comentarios;
-  
+  {
+    id: 1,
+    nombre: "Jesús",
+    comentario: "Hermoso el lugar sin molestias y la comida espectacular",
+    fecha: "Hace un día",
+    estrellas: 5,
+    ruta: "https://g.co/kgs/ZAup16L",
+  },
+  {
+    id: 2,
+    nombre: "Graciela Caramanica",
+    comentario:
+      "Muy buena atención de los dueños, muy amables y predispuestos. Las habitaciones cómodas y luminosas, la nuestra con balcón. A 3 cuadras del centro y al lado de una casa de empanadas excelente. Óptima relación precio y calidad. Los dueños nos solucionaron todo. Rico desayuno y hermoso el salón.",
+    fecha: "Hace 4 meses",
+    estrellas: 5,
+    ruta: "https://g.co/kgs/jFcRAJU",
+  },
+  {
+    id: 3,
+    nombre: "Ana Maria Romero",
+    comentario: "Excelente servicio por su propia dueña.",
+    fecha: "Hace 5 meses",
+    estrellas: 4,
+    ruta: "https://g.co/kgs/JNW7ZWL",
+  },
+  {
+    id: 4,
+    nombre: "Johanna Acosta",
+    comentario:
+      "Todo estuvo perfecto, el dueño está presente lo que hace que todo sea más cálido. La habitación, camas, baño todo super limpio, prolijo y cómodo. Pasamos una linda estadía. Relación precio/servicio super bien.",
+    fecha: "Hace 9 meses",
+    estrellas: 5,
+    ruta: "https://g.co/kgs/quEGv1r",
+  },
+];
+
+export default comentarios;
