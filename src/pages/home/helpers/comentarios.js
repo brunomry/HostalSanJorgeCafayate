9cbad4dfbe00bd817dee5d86189b358a9655ab7e
Bruno Madozzo <brunomadozzo@gmail.com>
2025-02-16 09:31:28 -0300

    actualice comentarios

--- src/pages/home/helpers/comentarios.js
+++ src/pages/home/helpers/comentarios.js
@@ -6,7 +6,7 @@ const comentarios = [
                   Ubicacion cerca de la plaza, muy buena, especialmente destaco la gran y calida atencion de los dueños que siempre que fui son buenos.
                   Las habitaciones muy lindas, el ventilador de techo enfria como aire acondicionado, es maravilloso. El baño espacioso y comodo, el desayuno muy bueno y agradable.
                   Recomendado si quieres venir con familia`,
-    fecha: "Hace una semana",
+    fecha: "Hace 1 mes",
     estrellas: 5,
     ruta: "https://goo.gl/maps/PA4RvqeLK1T6jB6W7",
   },
@@ -14,7 +14,7 @@ const comentarios = [
     id: 2,
     nombre: "Jesús",
     comentario: "Hermoso el lugar sin molestias y la comida espectacular",
-    fecha: "Hace 2 semanas",
+    fecha: "Hace 1 mes",
     estrellas: 5,
     ruta: "https://g.co/kgs/ZAup16L",
   },
@@ -23,7 +23,7 @@ const comentarios = [
     nombre: "Graciela Caramanica",
     comentario:
       "Muy buena atención de los dueños, muy amables y predispuestos. Las habitaciones cómodas y luminosas, la nuestra con balcón. A 3 cuadras del centro y al lado de una casa de empanadas excelente. Óptima relación precio y calidad. Los dueños nos solucionaron todo. Rico desayuno y hermoso el salón.",
-    fecha: "Hace 4 meses",
+    fecha: "Hace 5 meses",
     estrellas: 5,
     ruta: "https://g.co/kgs/jFcRAJU",
   },
@@ -31,7 +31,7 @@ const comentarios = [
     id: 4,
     nombre: "Ana Maria Romero",
     comentario: "Excelente servicio por su propia dueña.",
-    fecha: "Hace 5 meses",
+    fecha: "Hace 6 meses",
     estrellas: 4,
     ruta: "https://g.co/kgs/JNW7ZWL",
   },
@@ -40,7 +40,7 @@ const comentarios = [
     nombre: "Johanna Acosta",
     comentario:
       "Todo estuvo perfecto, el dueño está presente lo que hace que todo sea más cálido. La habitación, camas, baño todo super limpio, prolijo y cómodo. Pasamos una linda estadía. Relación precio/servicio super bien.",
-    fecha: "Hace 9 meses",
+    fecha: "Hace 10 meses",
     estrellas: 5,
     ruta: "https://g.co/kgs/quEGv1r",
   },
